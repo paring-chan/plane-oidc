d80dab6ed31f0bb313b759c91a77b55f843edfc2
Torben Raab <85336942+torbenraab@users.noreply.github.com>
2024-01-28 20:21:00 +0100

    Changes to use this repo as the source for the code when deploying as selfhosted application

--- deploy/1-click/install.sh
+++ deploy/1-click/install.sh
@@ -3,11 +3,11 @@
 if command -v curl &> /dev/null; then
     sudo curl -sSL \
         -o /usr/local/bin/plane-app \
-        https://raw.githubusercontent.com/makeplane/plane/${BRANCH:-master}/deploy/1-click/plane-app?token=$(date +%s)
+        https://raw.githubusercontent.com/torbenraab/plane/${BRANCH:-master}/deploy/1-click/plane-app?token=$(date +%s)
 else 
     sudo wget -q \
         -O /usr/local/bin/plane-app \
-        https://raw.githubusercontent.com/makeplane/plane/${BRANCH:-master}/deploy/1-click/plane-app?token=$(date +%s)
+        https://raw.githubusercontent.com/torbenraab/plane/${BRANCH:-master}/deploy/1-click/plane-app?token=$(date +%s)
 fi
 
 sudo chmod +x /usr/local/bin/plane-app
